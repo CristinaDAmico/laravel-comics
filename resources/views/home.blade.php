fcf5d68b91242a3c46abb367919f13dfda3d6fb8
CristinaDAmico <crydamico94@gmail.com>
2021-06-08 23:00:29 +0200

    commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,16 @@
 @extends('layouts.main')
 
 @section('content')
-    <section class="container">
-
-        <h1>Homepage</h1>
-        
-    </section> 
+<section class="comics">
+    <div class="container">
+        <div class="cards d-flex">
+            @foreach ($cards as $card)
+                <div class="card">
+                    <img src="{{ $card['image']}}" alt="">
+                    <h3 class="card-title">{{ $card['title']}}</h3>
+                </div>
+            @endforeach
+        </div>
+    </div>
+</section>
 @endsection
